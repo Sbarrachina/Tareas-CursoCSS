cc279495a1a561256ab8806e2ca4796e5ef6b5ba
Sbarrachina <sandrabarrachina1978@gmail.com>
2022-11-19 20:42:43 +0100

    creada pagina completa de halloween, falta adaptarla a temática navideña, escribir historias

--- noel_web/js/scripts.js
+++ noel_web/js/scripts.js
@@ -2,3 +2,11 @@
 // initialized AOS Librery
 AOS.init();
 
+
+let menu_bar = document.getElementById('menu_bar');
+let menu = document.getElementById('menu');
+
+menu_bar.addEventListener('click',function(){
+menu.classList.toggle('show-menu')
+
+})
